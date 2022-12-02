6f07db67643abeb475e27f7b68ebf81bb493bb8e
PatrykSech <119355037+PatrykSech@users.noreply.github.com>
2022-12-03 00:45:07 +0100

    js code refactoring

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-let formElement = document.querySelector(".js-form");
-let formAmount = document.querySelector(".js-amount");
-let formCurrency = document.querySelector(".js-currency");
-let formResult = document.querySelector(".js-result");
-
-let USD = 4.5;
-let AUD = 3.1;
-let GBP = 5.5;
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let amount = formAmount.value;
-    let currency = formCurrency.value;
-
-    let result;
+const calculateResults = (amount, currency) => {
+    const USD = 4.5;
+    const AUD = 3.1;
+    const GBP = 5.5;
 
     switch (currency) {
         case 'USD':
-            result = amount / USD;
-            break;
+            return amount / USD;
 
         case 'AUD':
-            result = amount / AUD;
-            break;
+            return amount / AUD;
 
         case 'GBP':
-            result = amount / GBP;
-            break;
+            return amount / GBP;
     }
+};
+
+const upDateResultsText = (amount, result, currency) => {
+    const formResult = document.querySelector(".js-result");
 
     formResult.innerText = `${result.toFixed(2)} ${currency}`
+};
 
-});
+const onFormSubmit = (event) => {
+        event.preventDefault();
+        const formAmount = document.querySelector(".js-amount");
+        const formCurrency = document.querySelector(".js-currency");
+
+        const amount = formAmount.value;
+        const currency = formCurrency.value;
+
+        let result = calculateResults(amount, currency);
+
+        upDateResultsText (amount, result, currency);
+};
+
+const init = () => {
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", onFormSubmit);
+};
+
+init ();
